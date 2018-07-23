f3874a6b76656e99c546af93e1edb64cd3942c2e
alexandrawowlol <alexandra.hurtado03@gmail.com>
2018-07-23 21:25:23 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,17 @@
 
 # Code your methods below
+def plant legs far apart, bend knees slightly and keep posture loose end
+  def lift right foot 
+    def return right foot to the ground end
+    def finishing with a small skip step backward end
+    def skip-step (left) end
+      def bounce left foot back up slightly, kicking it a few inches back end
+        def left, right, left end
+          def skip-step (right) end
+            def lower right foot to the ground end
+              def bounce right foot back up slightly, kicking it a few inches back end
+                def right, left, right and right end
+                  def holding your arms out in front of you, straight at chest level end
+                    def cross your right wrist over your left and hold them together end
+                      def lift your arms up and down time with the beat of the song end
+                        def this movement is repeated eight times end
